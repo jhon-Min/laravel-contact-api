d53909842ddd12ba49c20d86dcf5b12f91a55079
jhon-Min <minnyisay@gmail.com>
2022-03-24 02:23:09 +0630

    first commit

---
+++ app/Http/Controllers/ContactController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreContactRequest;
+use App\Http\Requests\UpdateContactRequest;
+use App\Models\Contact;
+
+class ContactController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $contacts = Contact::latest('id')->get();
+        return response()->json($contacts, 200);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\StoreContactRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreContactRequest $request)
+    {
+        $contact = new Contact();
+        $contact->name = $request->name ? $request->name : 'Unknown';
+        $contact->phone = $request->phone;
+        $contact->save();
+
+        return response()->json($contact, 200);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Contact  $contact
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $contact = Contact::find($id);
+        if (is_null($contact)) {
+            return response()->json(["message" => "No contact with this data"], 404);
+        }
+        return response()->json($contact, 200);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\UpdateContactRequest  $request
+     * @param  \App\Models\Contact  $contact
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdateContactRequest $request, Contact $contact)
+    {
+        if ($request->has('name')) {
+            $contact->name = $request->name;
+        }
+
+        if ($request->has('phone')) {
+            $contact->phone = $request->phone;
+        }
+
+        $contact->update();
+        return response()->json($contact, 200);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Contact  $contact
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Contact $contact)
+    {
+        $contact->delete();
+        return response()->json('', 204);
+    }
+}
